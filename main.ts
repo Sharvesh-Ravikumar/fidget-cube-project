3a9b924a0d3e80a81cd5810af01a0fa7e9f51a03
Sharvesh-Ravikumar <122555514+Sharvesh-Ravikumar@users.noreply.github.com>
2023-01-19 12:24:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,158 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # . . # .
+        # . . # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # . . # .
+        # . . # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # . . # .
+        # . . # .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # # # . .
+        . . . # .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . # . .
+        # # # . .
+        # . # . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+basic.showIcon(IconNames.StickFigure)
+basic.forever(function () {
+	
+})
